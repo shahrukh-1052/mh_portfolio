d8572340234fd65197398750e2221d2cb6011e85
Ayushman Sarkar <ayushhmansarkar@gmail.com>
2025-06-11 02:00:50 +0530

    Added option to view resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = () => {
               } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(nav.title)}
             >
-              <a href={`#${nav.id}`}>{nav.title}</a>
+              <a href={nav.id ==='resume'? "https://drive.google.com/file/d/1WieqCvRogTZLRHiAG7AdcDYPJNnDOzSo/view?usp=drive_link" : `#${nav.id}`}>{nav.title}</a>
             </li>
           ))}
         </ul>
@@ -88,7 +88,9 @@ const Navbar = () => {
                     setActive(nav.title);
                   }}
                 >
-                  <a href={`#${nav.id}`}>{nav.title}</a>
+                  <a href={nav.id==='resume' ? "https://drive.google.com/file/d/1WieqCvRogTZLRHiAG7AdcDYPJNnDOzSo/view?usp=drive_link" : `#${nav.id}`}>
+                    {nav.title}
+                  </a>
                 </li>
               ))}
             </ul>
--- src/constants/index.js
+++ src/constants/index.js
@@ -39,6 +39,10 @@ export const navLinks = [
     id: "contact",
     title: "Contact",
   },
+    {
+    id: "resume",
+    title: "Resume",
+  },
 ];
 
 const services = [
